d6bc6c20ee40d97d55d34f9ff2f90c84166ccc11
Gameckh <chenkaihua_kevin@foxmail.com>
2025-05-02 17:14:06 +0800

    数据库变更为云端数据库

--- src/config/mongodb.config.ts
+++ src/config/mongodb.config.ts
@@ -2,9 +2,13 @@ import { env, envNumber } from '~/global/env'
 
 export const mongDBRegToken = 'mongDB'
 
+// 加入用户名密码认证信息
 export const mongDBConfig = {
   type: 'mongodb',
   host: env('DB_MONGODB_URL', '127.0.0.1'), // MongoDB 连接 URL
   port: envNumber('DB_MONGODB_PORT', 27017),
   database: env('DB_MONGODB_DATABASE'), // 数据库名称
+  username: env('DB_MONGODB_USERNAME'), // 用户名
+  password: env('DB_MONGODB_PASSWORD'), // 密码
 }
+// mongodb://root:26jkue3N5QxcFyw2ysyX@113.44.53.177:27017/nest_admin
--- src/monogdb/connect.ts
+++ src/monogdb/connect.ts
@@ -4,8 +4,9 @@ import { mongDBConfig } from '~/config/mongodb.config'
 
 export async function connectDB(): Promise<void> {
   try {
-    // 'mongodb://localhost:27017/your_database';
-    const uri = `mongodb://${mongDBConfig.host}:${mongDBConfig.port}/${mongDBConfig.database}`
+    // 'mongodb://root:26jkue3N5QxcFyw2ysyX@113.44.53.177:27017/nest_admin?authSource=admin';
+    // 加入用户名密码认证信息
+    const uri = `mongodb://${mongDBConfig.username}:${mongDBConfig.password}@${mongDBConfig.host}:${mongDBConfig.port}/${mongDBConfig.database}?authSource=admin`
     await mongoose.connect(uri)
     console.log('MongoDB connected successfully.')
   }
