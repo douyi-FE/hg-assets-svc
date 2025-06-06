d6f0d75319cc3606c6b01aac944f6dce95547639
walker <wangjie0806@qq.com>
2025-06-06 19:43:01 +0800

    cad制图提交一点

--- src/monogdb/models/cad.ts
+++ src/monogdb/models/cad.ts
@@ -4,13 +4,19 @@ export interface ICad extends Document {
   name: string
   cadPath: string
   ejs: string
+  links: any[]
 }
 
 const cadSchema = new Schema({
   name: { type: String, required: true },
   cadPath: { type: String, required: false },
   ejs: { type: String, required: false },
-}, { timestamps: true })
+  links: { type: [Schema.Types.Mixed], require: false, default: [] },
+}, {
+  timestamps: true,
+  toObject: { getters: true }, // 启用读取转换
+  toJSON: { getters: true }, // 确保JSON输出也应用getter
+})
 
 const CadCollect = mongoose.model<ICad>('Cad', cadSchema, 'cad')
 
